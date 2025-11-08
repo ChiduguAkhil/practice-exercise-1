79b9f7a4c2a9d5ef2e43893620ac9c12c14aa34d
ChiduguAkhil <chiakhil123@gmail.com>
2025-11-08 19:10:12 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
         System.out.print("Enter two numbers: ");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int a = 2;
+        int b = 3;
         int sum = a + b;
         System.out.println("Sum: " + sum);
-        sc.close();
+        //sc.close();
     }
 }
