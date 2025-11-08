07d75e0a48cdb28dce71d53f1dda42658dbdf32a
ChiduguAkhil <chiakhil123@gmail.com>
2025-11-08 19:11:07 +0530

    Comment out user input prompt in AddTwoNumbers

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class AddTwoNumbers {
     public static void main(String[] args) {
         //Scanner sc = new Scanner(System.in);
-        System.out.print("Enter two numbers: ");
+        //System.out.print("Enter two numbers: ");
         int a = 2;
         int b = 3;
         int sum = a + b;
